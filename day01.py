e2dc6f8f18e1e6edbf47f3938d1d2786e7f524e1
Matt Cummins <18335242+fullptr@users.noreply.github.com>
2023-12-03 02:00:38 +0000

    Day 1

---
+++ day01.py
@@ -0,0 +1,28 @@
+with open("day01_input.txt") as f:
+    lines = f.readlines()
+    
+mapping = {
+    "one": "o1e",
+    "two": "t2o",
+    "three": "t3e",
+    "four": "f4r",
+    "five": "f5e",
+    "six": "s6x",
+    "seven": "s7n",
+    "eight": "e8t",
+    "nine": "n9e",
+}
+
+total_part1 = 0
+total_part2 = 0
+for line in lines:
+    ints = [i for i in line if i.isdigit()]
+    total_part1 += int(f"{ints[0]}{ints[-1]}")
+    
+    for key, val in mapping.items():
+        line = line.replace(key, val)
+        
+    ints = [i for i in line if i.isdigit()]
+    total_part2 += int(f"{ints[0]}{ints[-1]}")
+    
+print(total_part1, total_part2)
