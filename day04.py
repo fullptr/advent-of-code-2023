8bce567f603948fd1363a9b8201f95197aef03d1
Matt Cummins <18335242+fullptr@users.noreply.github.com>
2023-12-05 00:42:55 +0000

    Clean up day4

--- day04.py
+++ day04.py
@@ -1,15 +1,12 @@
 with open("day04_input.txt") as f:
     data = f.read().split("\n")
     
-games = []
-for line in data:
-    winning, ours = line.split(": ")[1].split(" | ")
-    into_set = lambda s: {int(x) for x in s.split()}
-    games.append(len(into_set(winning) & into_set(ours)))
-    
 part1 = 0
-part2 = [1 for _ in games]
-for idx, count in enumerate(games):
+part2 = [1 for _ in data]
+for idx, line in enumerate(data):
+    winning, ours = line.split(": ")[1].split(" | ")
+    count = len(set(winning.split()) & set(ours.split()))
+    
     part1 += 2 ** (count - 1) if count != 0 else 0
     for offset in range(count):
         part2[idx + 1 + offset] += part2[idx]
