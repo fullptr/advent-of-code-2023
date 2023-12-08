a9d3f4c76f4298406e71ad08095b41cc16fbbc66
Matt Cummins <18335242+fullptr@users.noreply.github.com>
2023-12-08 22:36:02 +0000

    Solve day8

---
+++ day08.py
@@ -0,0 +1,31 @@
+with open("day08_input.txt") as f:
+    directions, edges = f.read().split("\n\n")
+    
+graph = {}
+for line in edges.split("\n"):
+    src, dsts = line.split(" = ")
+    graph[src] = dsts[1:-1].split(", ")
+    
+from itertools import cycle
+from math import lcm
+
+def get_cycle(start, is_sentinel):
+    curr = start
+    count = 0
+    for d in cycle(directions):
+        if is_sentinel(curr):
+            return count
+        
+        if d == "L":
+            curr = graph[curr][0]
+        else:
+            curr = graph[curr][1]
+        count += 1
+      
+part1_sentinel = lambda n: n == "ZZZ"  
+part1 = get_cycle("AAA", part1_sentinel)
+
+part2_sentinel = lambda n: n.endswith("Z")
+part2 = lcm(*[get_cycle(n, part2_sentinel) for n in graph if n.endswith("A")])
+
+print(part1, part2)
